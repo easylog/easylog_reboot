e4500946ab2be8bbd83b1061fdcc758fe67d42f1
easylog <easyloghilfe@gmail.com>
2025-04-20 16:10:41 +0200

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push('/auth/login');
-  }, []);
-  return null;
-}
